905e7fccf645c281267988281bade3b8bdf25716
AxelSalim <adjakidjememiaghe@gmail.com>
2024-11-27 11:06:29 +0100

    modification du layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
   <title>{{ env('APP_NAME') }}</title>
   <!-- Alpinejs -->
   <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-  
+
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-slate-100 text-slate-900">
@@ -18,22 +18,25 @@
       <a href="" class="nav-link">Home</a>
 
       @auth
-          
-      @endauth
-
-      @auth
-        <div class="relative grid place-items-center">
+        <div class="relative grid place-items-center" x-data="{ open: false }">
           {{-- Dropdown menu button --}}
-          <button type="button" class="round-btn">
+          <button x-on:click="open = !open" type="button" class="round-btn">
             <img src="https://picsum.photos/200" alt="">
           </button>
+
+          {{-- Dropdown menu --}}
+          <div x-show="open" @click.outside="open = false" class="bg-white shadow-lg absolute top-10 right-0 rounded-lg overflow-hidden font-light">
+            <p class="username py-2">{{ auth()->user()->username }}</p>
+            <hr>
+            <a href="{{ route('dashboard') }}" class="block hover:bg-slate-100 pl-4 pr-8 py-2 mb-1 font-medium">Dashboard</a>
+
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button class="block hover:bg-slate-100 pl-4 pr-8 py-2 mb-1 font-medium w-full text-left">Logout</button>
+            </form>
+        </div>
         </div>
 
-        {{-- Dropdown menu --}}
-        <div class="bg-white shadow-lg absolute top-10 right-0 rounded-lg overflow-hidden font-light">
-          <p class="username">username</p>
-          <a href="" class="block hover:bg-slate-100 pl-4 pr-8 py-8 mb-1">Dashboard</a>
-        </div>
       @endauth
 
       @guest
