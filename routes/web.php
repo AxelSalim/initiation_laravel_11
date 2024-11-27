054563c68b7fb5be7519ff8d2ae5ea8489a3e9a7
AxelSalim <adjakidjememiaghe@gmail.com>
2024-11-27 10:01:30 +0100

    ajout de la route pour l'affichage du dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -11,9 +12,11 @@ Route::fallback(function() {
 
 Route::view('/', 'posts.index')->name('home');
 
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::view('/register', 'auth.register')->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::view('/login', 'auth.login')->name('login');
 Route::post('/login', [AuthController::class, 'login']);
+
