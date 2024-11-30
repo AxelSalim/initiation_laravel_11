7684032405434e3d61fdeab9106b9a522a47790f
AxelSalim <adjakidjememiaghe@gmail.com>
2024-11-30 23:44:27 +0100

    restructuration des routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -11,12 +12,19 @@ Route::fallback(function() {
 })->name("404");
 
 Route::view('/', 'posts.index')->name('home');
+Route::resource('/posts', PostController::class);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::middleware('auth')->group(function() {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 
-Route::view('/register', 'auth.register')->name('register');
-Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('guest')->group(function() {
+    Route::view('/register', 'auth.register')->middleware('guest')->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
 
-Route::view('/login', 'auth.login')->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+    Route::view('/login', 'auth.login')->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
 
