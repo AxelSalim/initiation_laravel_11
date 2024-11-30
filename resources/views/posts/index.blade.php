489f4db12b58557551983c7cb133911ba018fd47
AxelSalim <adjakidjememiaghe@gmail.com>
2024-12-01 00:50:01 +0100

    eloquent collection

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,22 @@
 <x-layout>
 
-  @auth
-    <h1>Logged in </h1>
-  @endauth
+  <h1 class="title">Latest Posts</h1>
 
-  @guest
-      <h1>Guest</h1>
-  @endguest
+  <div class="grid grid-cols-2 gap-6">
+    @foreach ($posts as $post)
+      <div class="bg-white p-8 m-4 rounded-md">
+        {{-- Title --}}
+        <h2 class="font-semibold text-2xl">{{ $post->title }} </h2>
+        {{-- Author and Date --}}
+        <div class="text-xs font-light mb-4">
+          <span>Posted {{ $post->created_at->diffForHumans() }}</span>
+          <a href="" class="text-blue-500 font-medium">USERNAME</a>
+        </div>
+        {{-- Body --}}
+        <div class="text-md">
+            <p>{{ Str::words($post->body, 15) }} </p>
+        </div>
+      </div>
+    @endforeach
+  </div>
 </x-layout>
